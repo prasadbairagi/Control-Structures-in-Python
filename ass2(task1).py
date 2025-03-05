5a1f0523b0c382bd1576d01ea572840d92301af9
PRASAD BAIRAGI <prasadbairagi98@gmail.com>
2025-03-05 10:39:42 +0530

    Add files via upload

---
+++ ass2(task1).py
@@ -0,0 +1,7 @@
+n1=int(input("Enter The Number:"))
+
+if n1 % 2 == 0:
+    print(f" {n1} - This One is Even Number.")
+else:
+    print(f" {n1} - This One is Odd Number.")
+
---
+++ ass2(task2).py
@@ -0,0 +1,9 @@
+
+
+print("Sum of Integers from 1 to 50")
+
+sum = 0
+for i in range(1,51):
+    sum += i
+print(sum)
+
